03800560dd7f741b07fa7533ded24e7156c01271
LiFengcheng01 <1007430174@qq.com>
2017-11-05 13:36:46 +0800

    编程解决猫头猫爪问题

---
+++ Miao.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   int c,h,hand;
+    for(c=0;c<100;c++)
+        for(h=0;h<100;h++)
+          for(hand=0;hand<100;hand++)
+             if(c*3==30&&c+h*2==20&&h+4*hand==9)
+             cout<<h+(hand*(c-hand));
+    return 0;
+}
