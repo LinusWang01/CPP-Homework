80f84c2c392f792ca15874b1a0a8396cf6f2e334
LiFengcheng01 <1007430174@qq.com>
2017-11-20 19:05:35 -0500

    插入排序的作业应用

--- order.cpp
+++ order.cpp
@@ -1,53 +1,66 @@
 #include<iostream>
 using namespace std;
-int num[10];
-int in(int num0,int des);
-void fillall();
-void out();
+int insert(int num0,int des,int C[]);
+void fillAtoC(int C[],int A[]);
+void input(int A[]);
+void output(int A[]); 
 int main()
-{
+{ 
+  int A[10];
+  int B[10];
+  input(A);
+  input(B);
+  int C[20];
    int i,num0,j;
-   fillall();
+   fillAtoC(C,A);
    for(i=0;i<10;i++)
    {
-	
-	
-	cin>>num0;
-	for(j=0;j<10;j++)
-		{
-			if(num0<=num[j])
-			  {
-			   in(num0,j);
-		           break;
-		          }
-		}
-   }
-	out();
+        num0=B[i];
+        for(j=0;j<20;j++)
+        {
+            if(num0<=C[j])
+            {
+			 insert(num0,j,C);
+			 break;
+            }
+            if(j==19)
+            C[j]=num0; 
+            
+        }
+   } 
+        output(C);
+       
+		return 0; 
 }
-int in(int num0,int des)
+int insert(int num0,int des,int A[])
 {
-   int temp[20];
-   for(int i=0;i<10;i++)
-       temp[i]=num[i]; 
-   for(int i=des;i<9;i++)
-	{ 
-	temp[i+1]=num[i];
-	temp[des]=num0; 
-	}
-   for(int p=0;p<10;p++)
-	num[p]=temp[p]; 
-   for(int p=0;p<10;p++)
-	temp[p]=0;
+ int temp[30];
+   for(int i=0;i<20;i++)
+       temp[i]=A[i];
+   for(int i=des;i<20;i++)
+        {
+         temp[i+1]=A[i];
+         temp[des]=num0;
+        }
+   for(int p=0;p<20;p++)
+        A[p]=temp[p];
+   for(int p=0;p<20;p++)
+        temp[p]=0;
 }
-void out()
+void output(int A[])
 {
-   
-    for(int i=0;i<10;i++)
-	cout<<num[i]<<"  "; 
+    for(int i=0;i<20;i++)
+        cout<<A[i]<<"  ";
         cout<<endl;
 }
-void fillall()
+void fillAtoC(int C[],int A[])
 {
    for(int i=0;i<10;i++)
-       num[i]=2147483647;
+       C[i]=A[i];
+}
+
+void input(int A[])
+{
+	for(int i=0;i<10;i++)
+	cin>>A[i];
 }
