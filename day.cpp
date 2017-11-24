5ad39e974977d385ba1e67c0237d23559b958a3d
LiFengcheng01 <1007430174@qq.com>
2017-11-24 03:54:26 -0500

    calculate the day input

--- day.cpp
+++ day.cpp
@@ -5,22 +5,13 @@ int year,mon;
 int day,sum;
 int main()
 {
-cin>>year>>mon>>day;
-
-for(int i=0;i<mon-1;i++)
-{
-sum+=month[i];
-
-}
-sum=sum+day;
-if(((year%4==0)&&!(year%100))||(year%400==0))
-  sum=sum+1;
-
-
-cout<<sum<<endl;
-
-
-
-
-
+		
+		cin>>year>>mon>>day;
+	        for(int i=0;i<mon-1;i++)
+			sum+=month[i];
+                sum=sum+day;
+		if(((year%4==0)&&!(year%100))||(year%400==0))
+	  		sum=sum+1;
+		cout<<sum<<endl;
+	
 }
