c7d2779981880e27482ed3f0939ab69aa7687f06
FengchengLi <1007430174@qq.com>
2017-11-27 08:08:16 +0800

    实现了一个简单员工信息管理系统（Linux适配稍后实现）

--- employee.cpp
+++ employee.cpp
@@ -35,10 +35,10 @@ int main()
 
                 if (fs=='0')
                 {
-					system("cls");
+			system("cls");
                 	cout<<"#请输入信息,格式为：姓名 工号 工资,可输入多组数据，Ctrl+Z+Enter结束。"<<endl;;
                 	while(cin>>names[index]>>ecodes[index]>>sal[index])
-							{
+				{
                        	         index++;
                       	         if(index>99)
                        	                {
@@ -46,7 +46,7 @@ int main()
                                                 break;
                                         }
                         	}
-                    system("cls");
+                        system("cls");
         		}
 
                 else if (fs=='1')
